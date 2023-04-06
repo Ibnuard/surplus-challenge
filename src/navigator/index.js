f546c9fffcc96f36802ebb3f12c308d74ed6227a
Ibnu Putra <azxarblog@gmail.com>
2023-04-06 17:52:34 +0700

    update navigation icon

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -1,11 +1,16 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// === OTHER
+import Icon from 'react-native-vector-icons/AntDesign';
+
+// === SCREEN ROUTE
 import HomeScreen from '../screens/home';
 import ProfileScreen from '../screens/profile';
 import BoardingScreen from '../screens/boarding';
 import SplashScreen from '../screens/splash';
 import SignInScreen from '../screens/signin';
 import SignUpScreen from '../screens/signup';
+import {Colors} from '../styles';
 
 //create stack screen
 const Stack = createNativeStackNavigator();
@@ -64,9 +69,23 @@ export const AuthStackScreen = () => {
 export const MainScreen = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Profile') {
+            iconName = 'meh';
+          }
+
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: Colors.COLOR_PRIMARY,
+        tabBarInactiveTintColor: Colors.COLOR_GRAY,
         tabBarHideOnKeyboard: true,
-      }}>
+      })}>
       <Stack.Screen
         name="Home"
         component={HomeScreen}
